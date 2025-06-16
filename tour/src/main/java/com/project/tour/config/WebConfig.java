0cbaff9c22ffb61813e438c61a130298f3843a43
gi-yun <kbb9554@gmail.com>
2025-06-16 21:38:26 +0900

    trb-001
    깃 에 기본설정및 테스트 (이그노어 등)
    trb(fourback) - 001 해서 계속 커밋할때 마다 번호 올려주길 바람

---
+++ tour/src/main/java/com/project/tour/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.project.tour.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 URL에 대해 허용
+                .allowedOrigins("http://localhost:5173") // React 개발 서버
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
