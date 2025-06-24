7410fa8d097852057865d9a9e4f4a3472d2ba8cd
JungMin <jmahn1819@gmail.com>
2025-06-25 04:21:23 +0900

    필요없는 임포트 지움

--- tour/src/main/java/com/project/tour/controller/TourController.java
+++ tour/src/main/java/com/project/tour/controller/TourController.java
@@ -3,7 +3,6 @@ package com.project.tour.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
